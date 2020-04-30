596cb785cfb70191d35fabaf7ff94e76ba4130c2
huchengzhen <huchengzhen@gmail.com>
2020-05-01 01:15:20 +0800

    Clean workspace

--- src/main/java/com/huchengzhen/weblog/security/JWTAuthorizationFilter.java
+++ src/main/java/com/huchengzhen/weblog/security/JWTAuthorizationFilter.java
@@ -1,6 +1,5 @@
 package com.huchengzhen.weblog.security;
 
-import com.huchengzhen.weblog.service.UserService;
 import com.huchengzhen.weblog.util.JwtTokenUtils;
 import io.jsonwebtoken.Claims;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -17,59 +16,53 @@ import java.io.IOException;
 
 public class JWTAuthorizationFilter extends BasicAuthenticationFilter {
 
-    private JwtTokenUtils jwtTokenUtils;
+	private JwtTokenUtils jwtTokenUtils;
 
-    private UserService userService;
+	public JWTAuthorizationFilter(AuthenticationManager authenticationManager, JwtTokenUtils jwtTokenUtils) {
+		super(authenticationManager);
+		this.jwtTokenUtils = jwtTokenUtils;
+	}
 
-    public JWTAuthorizationFilter(AuthenticationManager authenticationManager, JwtTokenUtils jwtTokenUtils, UserService userService) {
-        super(authenticationManager);
-        this.jwtTokenUtils = jwtTokenUtils;
-        this.userService = userService;
-    }
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
 
-    @Override
-    protected void doFilterInternal(HttpServletRequest request,
-                                    HttpServletResponse response,
-                                    FilterChain chain) throws IOException, ServletException {
+		String tokenHeader = request.getHeader(JwtTokenUtils.TOKEN_HEADER);
+		// 如果请求头中没有Authorization信息则直接放行了
+		if (tokenHeader == null || !tokenHeader.startsWith(JwtTokenUtils.TOKEN_PREFIX)) {
+			chain.doFilter(request, response);
+			return;
+		}
+		// 如果请求头中有token，则进行解析，并且设置认证信息
+		SecurityContextHolder.getContext().setAuthentication(getAuthentication(tokenHeader));
 
-        String tokenHeader = request.getHeader(JwtTokenUtils.TOKEN_HEADER);
-        // 如果请求头中没有Authorization信息则直接放行了
-        if (tokenHeader == null || !tokenHeader.startsWith(JwtTokenUtils.TOKEN_PREFIX)) {
-            chain.doFilter(request, response);
-            return;
-        }
-        // 如果请求头中有token，则进行解析，并且设置认证信息
-        SecurityContextHolder.getContext().setAuthentication(getAuthentication(tokenHeader));
-
-
-        super.doFilterInternal(request, response, chain);
+		super.doFilterInternal(request, response, chain);
 //        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 //        System.out.println("header"+ SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString());
 //        System.out.println("header"+ SecurityContextHolder.getContext().getAuthentication().getAuthorities().toString());
 
+	}
 
-    }
+	// 这里从token中获取用户信息并新建一个token
+	private UsernamePasswordAuthenticationToken getAuthentication(String tokenHeader) {
+		if (tokenHeader.startsWith(JwtTokenUtils.TOKEN_PREFIX)) {
+			String tokenString = tokenHeader.substring(JwtTokenUtils.TOKEN_PREFIX.length());
 
-    // 这里从token中获取用户信息并新建一个token
-    private UsernamePasswordAuthenticationToken getAuthentication(String tokenHeader) {
-        if (tokenHeader.startsWith(JwtTokenUtils.TOKEN_PREFIX)) {
-            String tokenString = tokenHeader.substring(JwtTokenUtils.TOKEN_PREFIX.length());
+			Claims claims = jwtTokenUtils.getTokenBody(tokenString);
+			String username = jwtTokenUtils.getUsername(claims);
 
-            Claims claims = jwtTokenUtils.getTokenBody(tokenString);
-            String username = jwtTokenUtils.getUsername(claims);
-
-            if (username != null) {
-                String roles = jwtTokenUtils.getUserRole(claims);
-                Long id = jwtTokenUtils.getId(claims);
+			if (username != null) {
+				String roles = jwtTokenUtils.getUserRole(claims);
+				Long id = jwtTokenUtils.getId(claims);
 //                userService.updateLastLoginDate(id, new Date());
-                UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(username, null,
-                        AuthorityUtils.commaSeparatedStringToAuthorityList(roles));
+				UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(username, null,
+						AuthorityUtils.commaSeparatedStringToAuthorityList(roles));
 
-                token.setDetails(id);
-                return token;
-            }
-        }
+				token.setDetails(id);
+				return token;
+			}
+		}
 
-        return null;
-    }
+		return null;
+	}
 }
